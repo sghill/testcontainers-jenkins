c166cfb0e1a38464ac464a711e131d9ae1bb97e5
Steve Hill <sghill.dev@gmail.com>
2023-06-24 09:06:18 -0700

    Switch to dependabot-friendly version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,17 +6,19 @@ group = "net.sghill.testcontainers"
 
 dependencies {
     compileOnlyApi(libs.auto.value.annotations)
-    annotationProcessor(libs.auto.value)
-    api("org.testcontainers:testcontainers:1.18.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.+")
+    annotationProcessor(libs.auto.value.processor)
+    api(platform(libs.testcontainers.bom))
+    api(libs.testcontainers.core)
+    implementation(platform(libs.jackson.bom))
+    implementation(libs.jackson.databind)
 
     testCompileOnly(libs.auto.value.annotations)
-    testAnnotationProcessor(libs.auto.value)
-    testImplementation("org.assertj:assertj-core:3.+")
-    testImplementation("io.rest-assured:rest-assured:5.3.0")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.awaitility:awaitility:4.+")
-    testRuntimeOnly("org.slf4j:slf4j-simple:1.7.36")
+    testAnnotationProcessor(libs.auto.value.processor)
+    testImplementation(libs.assertj.core)
+    testImplementation(libs.rest.assured)
+    testImplementation(libs.junit4)
+    testImplementation(libs.awaitility)
+    testRuntimeOnly(libs.slf4j.simple)
 }
 
 java {
